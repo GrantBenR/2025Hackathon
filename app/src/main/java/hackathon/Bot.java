ed46afdf7ec43008040054146a2286ea6a88b817
Nathan Fairweather <nfairweather93@gmail.com>
2025-01-31 17:54:44 -0500

    Added info to Bot.java

--- app/src/main/java/hackathon/Bot.java
+++ app/src/main/java/hackathon/Bot.java
@@ -1,7 +1,26 @@
 package hackathon;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Bot {
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        //Grab the bot token from the .env file so it doesn't get pushed to github
+        Dotenv dotenv = Dotenv.load();
+        String BOT_TOKEN = dotenv.get("BOT_TOKEN");
+
+        //Var for command prefix
+        String prefix = "/";
+
+        //Set up the JDA API with the bot
+        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+
+        //Space to register listeners
+
+        //Space for slash commands
+
     }
 }
