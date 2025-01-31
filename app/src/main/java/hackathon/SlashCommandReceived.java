5b0aad8d709549fdd03bb125ae010a6c7255e785
Nathan Fairweather <nfairweather93@gmail.com>
2025-01-31 18:17:02 -0500

    Commenting

--- app/src/main/java/hackathon/SlashCommandReceived.java
+++ app/src/main/java/hackathon/SlashCommandReceived.java
@@ -4,17 +4,25 @@ package hackathon;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
+/**
+ * Class used to track slash commands used by the discord bot
+ * @author Nathan Fairweather
+ */
 public class SlashCommandReceived extends ListenerAdapter{
 
+    //Pull the command prefix from  bot class
     public static String prefix = Bot.prefix;
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
         if(event.getMember().getUser().isBot()) return;
 
+        //Command for recent songs (Spotify)
         if(event.getName().equalsIgnoreCase("RecentSongs")){
                 event.reply("Your most recent listened to songs were:\n1: \n2: \n3: \n4: \n5: ").setEphemeral(false).queue();
         }
+
+        //Command for recent artists (spotify)
         if(event.getName().equalsIgnoreCase("RecentArtists")){
             event.reply("Your most recent listened to songs were:\n1: \n2: \n3: \n4: \n5: ").setEphemeral(false).queue();
         }
