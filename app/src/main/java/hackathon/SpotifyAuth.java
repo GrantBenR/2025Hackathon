d32069586f0e01665106bea75f93c6e4da33b603
GrantBenR <thegrantrynders@gmail.com>
2025-01-31 18:44:56 -0500

    Code Verifier and Code Challenge Funcs

--- app/src/main/java/hackathon/SpotifyAuth.java
+++ app/src/main/java/hackathon/SpotifyAuth.java
@@ -1,16 +1,55 @@
 package hackathon;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SpotifyAuth
 {
-    private String CLIENT_ID;
-    private String CLIENT_SECRET;
     public SpotifyAuth()
     {
+        //Get spotify dev client data from .env
         Dotenv dotenv = Dotenv.load();
-        CLIENT_ID = dotenv.get("CLIENT_ID");
-        CLIENT_SECRET = dotenv.get("CLIENT_ID");
+        String CLIENT_ID = dotenv.get("CLIENT_ID");
+        String CLIENT_SECRET = dotenv.get("CLIENT_ID");
+        //Length indicated by spotify documentation
+        String codeVerifier = generateCodeVerifier(128);
+        String codeChallenger = generateCodeChallenge(codeVerifier);
 
+    }
+    public String generateCodeVerifier(int length) {
+        String text = "";
+        String possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        int possibleLength = possible.length();
+        for (int i = 0; i < length; i++) {
+            text += possible.charAt((int) Math.floor(Math.random() * possibleLength));
+        }
+        return text;
+    }
+    public String generateCodeChallenge(String codeVerifier) {
+        MessageDigest digest = null;
+        try
+        {
+            digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedVerifier = digest.digest(codeVerifier.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder(2 * encodedVerifier.length);
+            for (byte b : encodedVerifier) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            String outputString = hexString.toString();
+            System.out.println("SHA-256 Hash: " + outputString);
+            return outputString;
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            System.out.println("Error in Message Digest Get Instance SHA 256 in SpotifyAuth.java");
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
