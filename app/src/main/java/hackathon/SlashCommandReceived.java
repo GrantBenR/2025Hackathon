40634f62b92fd292183d1704c25717c72b6ca47a
Nathan Fairweather <nfairweather93@gmail.com>
2025-01-31 19:34:41 -0500

    Try to sync commands

--- app/src/main/java/hackathon/SlashCommandReceived.java
+++ app/src/main/java/hackathon/SlashCommandReceived.java
@@ -2,7 +2,14 @@ package hackathon;
 
 //Imports
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+
+import java.util.*;
+import java.io.*;
 
 /**
  * Class used to track slash commands used by the discord bot
@@ -26,6 +33,18 @@ public class SlashCommandReceived extends ListenerAdapter{
         if(event.getName().equalsIgnoreCase("RecentArtists")){
             event.reply("Your most recent listened to songs were:\n1: \n2: \n3: \n4: \n5: ").setEphemeral(false).queue();
         }
+    }
+
+    // Global Command - Updates in about an hour - max 100
+    @Override
+    public void onReady(ReadyEvent event){
+        
+        List<CommandData> commandData = new ArrayList<>();
+        
+        commandData.add(Commands.slash("RecentArtists", "Imports a list of the most recent artists"));
+        commandData.add(Commands.slash("RecentSongs", "Imports a list of the most recent songs"));
+
+        event.getJDA().updateCommands().addCommands(commandData).queue();
     }
 }
 
