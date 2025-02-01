af253b181ed4487c648759ae926e3f8fa21c242d
GrantBenR <thegrantrynders@gmail.com>
2025-01-31 19:16:17 -0500

    Potentially working getAccessToken func

--- app/src/main/java/hackathon/SpotifyAuth.java
+++ app/src/main/java/hackathon/SpotifyAuth.java
@@ -1,12 +1,27 @@
 package hackathon;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SpotifyAuth
 {
+    //THIS IS DEFINITELY WRONG ---->>>
+    private final static String BASE_URL = "https://accounts.spotify.com/api/token";
     public SpotifyAuth()
     {
         //Get spotify dev client data from .env
@@ -16,9 +31,11 @@ public class SpotifyAuth
         //Length indicated by spotify documentation
         String codeVerifier = generateCodeVerifier(128);
         String codeChallenger = generateCodeChallenge(codeVerifier);
+        String accessToken = getAccessToken(CLIENT_ID, codeChallenger, codeVerifier);
 
     }
-    public String generateCodeVerifier(int length) {
+    public String generateCodeVerifier(int length)
+    {
         String text = "";
         String possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         int possibleLength = possible.length();
@@ -27,7 +44,8 @@ public class SpotifyAuth
         }
         return text;
     }
-    public String generateCodeChallenge(String codeVerifier) {
+    public String generateCodeChallenge(String codeVerifier)
+    {
         MessageDigest digest = null;
         try
         {
@@ -50,6 +68,58 @@ public class SpotifyAuth
             System.out.println("Error in Message Digest Get Instance SHA 256 in SpotifyAuth.java");
             throw new RuntimeException(e);
         }
-        return null;
+    }
+    public String getAccessToken(String clientId, String code, String codeVerifier)
+    {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpGet httpGet = new HttpGet(BASE_URL);
+
+        try {
+            URI uri = new URIBuilder(httpGet.getURI())
+                    .addParameter("client_id", clientId)
+                    .addParameter("grant_type", "authorization_code")
+                    .addParameter("code", code)
+                    .addParameter("redirect_uri", "http://localhost:5500/callback")
+                    .addParameter("code_verifier", codeVerifier)
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString(uri.toString()))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String authToken = objectMapper.readValue(response.body(), new TypeReference<>() {});
+            System.out.println("Authentication request response: " + authToken);
+            return authToken;
+        }
+        catch (URISyntaxException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+//    const params = new URLSearchParams();
+//        params.append("client_id", clientId);
+//        params.append("grant_type", "authorization_code");
+//        params.append("code", code);
+//        params.append("redirect_uri", "http://localhost:5173/callback");
+//        params.append("code_verifier", verifier!);
+//
+//    const result = await fetch("https://accounts.spotify.com/api/token", {
+//                method: "POST",
+//                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//        body: params
+//    });
+//
+//    const { access_token } = await result.json();
+//        return access_token;
+//    }
     }
 }
